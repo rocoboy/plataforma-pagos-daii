8375fb9e67dd3b828fc924d3b89bb5f864a8310d
Juan Tarca <178902428+juanTarca@users.noreply.github.com>
2025-11-04 10:22:19 -0300

    Update create_payment.test.ts

--- apps/api/src/test/create_payment.test.ts
+++ apps/api/src/test/create_payment.test.ts
@@ -2,7 +2,7 @@ import { describe, expect, test } from "bun:test";
 
 describe("create payment webhook", () => {
   const baseUrl = Bun.env.API_BASE_URL ?? "http://localhost:3000";
-  const endpoint = new URL("/api/webhooks/payments", baseUrl).toString();
+  const endpoint = new URL("events", baseUrl).toString();
 
   test("should call create payment webhook endpoint", async () => {
 
@@ -11,14 +11,26 @@ describe("create payment webhook", () => {
     const randUserId = crypto.randomUUID();
     const randAmount = Math.floor(Math.random() * 10000) + 1;
 
-    const payload = {
-      res_id: randResId,
-      user_id: randUserId,
+    const innerPaymentData = { // datos nuestros de pago
+      paymentId: `pay-${crypto.randomUUID()}`,
+      reservationId: randResId,
+      userId: randUserId,
+      status: "SUCCESS",
       amount: randAmount,
       currency: "USD",
-      meta: {
-        reference: "bun-test",
-      },
+      updatedAt: new Date().toISOString(),
+    };
+
+    const payload = { //esto lo saque de otros mensajes que ya mandamos para poder ver porque no funcionaba
+      messageId: `msg-${crypto.randomUUID()}`,
+      eventType: "payments.payment.status_updated",
+      schemaVersion: "1.0",
+      occurredAt: new Date().toISOString(),
+      producer: "bun-test",
+      correlationId: `corr-${crypto.randomUUID()}`,
+      idempotencyKey: `idem-${crypto.randomUUID()}`,
+      
+      payload: JSON.stringify(innerPaymentData),
     };
 
     console.log("➡️  Calling", endpoint, "with payload:", payload);
@@ -28,6 +40,7 @@ describe("create payment webhook", () => {
       headers: {
         "Content-Type": "application/json",
         "Origin": "http://localhost:3000",
+        "X-API-Key": "microservices-api-key-2024-secure" //esto es lo que tenemos que cambiar
       },
       body: JSON.stringify(payload),
     });
